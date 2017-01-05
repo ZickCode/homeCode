4e3abaeea04bc7c7f6d249f9daeb9e45297431d0
zickcode <zickcode@foxmail.com>
2017-01-05 15:27:48 +0800

    commit

---
+++ vue-demo/src/component/firstComponent.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="firstComponent">
+		<h1>Test Vue </h1>
+		<h2>{{aaa}}</h2>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		name: 'firstComponent',
+		data () {
+			return {
+				aaa: 'Hello Zick'
+			}
+		}
+	}
+</script>
+
+<style type="text/css">
+	
+</style>
