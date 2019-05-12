de84e49595dcbe0c5afc95987c41613ecc0fb918
zickcode <zickcode@foxmail.com>
2019-05-12 15:42:25 +0800

    微信小程序外包

---
+++ testVueCli/src/components/test.vue
@@ -0,0 +1,17 @@
+<style>
+
+</style>
+
+<template>
+    <div>{{testMsg}}</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            testMsg: '666'
+        }
+    }
+}
+</script>
